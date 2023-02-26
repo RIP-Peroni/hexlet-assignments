db48f17929f2ac42149e765a7ac85b47e8b65063
@hexlet/cli <support@hexlet.io>
2023-02-26 23:52:11 +0300

    submit java-collections-ru/streams

--- java-collections-ru/streams/src/main/java/exercise/App.java
+++ java-collections-ru/streams/src/main/java/exercise/App.java
@@ -13,11 +13,8 @@ public class App {
     public static int getCountOfFreeEmails(List<String> emails) {
         List<String> freeHostsList = Arrays.asList(freeHosts);
         return (int) emails.stream()
-                .filter(email -> {
-                    String[] currentHostArray = email.split("@");
-                    String currentHost = currentHostArray[1];
-                    return freeHostsList.contains(currentHost);
-                })
+                .map(email -> email.split("@")[1])
+                .filter(freeHostsList::contains)
                 .count();
     }
 }
